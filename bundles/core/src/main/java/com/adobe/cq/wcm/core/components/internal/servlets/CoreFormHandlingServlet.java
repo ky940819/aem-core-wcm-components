64fa541efdec85c05d7f3a6fc2e9bd297f6e368e
Kyle Giovannetti <kyle.giovannetti@dal.ca>
2020-08-20 13:38:33 -0300

    Update core form handling servlet to validate captcha
    Updates the `CoreFormHanldingServlet` to validate that a captcha
    response is present and valid if required, prior to processing the
    form POST request.
    
    If the captcha is required but not present, or invalid, then a
    `forbidden` status code is returned.
    
    Update tests for `CoreFormHandlingServlet`
    
    Updates tests to verify the proper handling of the request
    by the servlet when the captcha is:
     1) not required
     2) required by component property; and,
       a) is valid
       b) is not valid
     3) required by policy; and,
       a) is valid
       b) is not valid
    
    Add JavaDocs to undocumented members of `CoreFormHandlingServlet`
    
    Adds JavaDoc comments to all undocumented internal members of the
    `CoreFormHandlingServlet` class.

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/servlets/CoreFormHandlingServlet.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/servlets/CoreFormHandlingServlet.java
@@ -16,6 +16,9 @@
 package com.adobe.cq.wcm.core.components.internal.servlets;
 
 import java.io.IOException;
+import java.util.Optional;
+import java.util.function.Supplier;
+import java.util.stream.StreamSupport;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -23,12 +26,20 @@ import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
 
+import com.adobe.cq.wcm.core.components.models.form.Captcha;
+import com.adobe.cq.wcm.core.components.services.captcha.CaptchaValidatorFactory;
+import com.day.cq.wcm.api.policies.ContentPolicy;
+import com.day.cq.wcm.api.policies.ContentPolicyManager;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.HttpConstants;
+import org.apache.sling.api.servlets.ServletResolverConstants;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
-import org.apache.sling.commons.osgi.PropertiesUtil;
+import org.apache.sling.engine.EngineConstants;
 import org.jetbrains.annotations.NotNull;
+import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -50,75 +61,154 @@ import com.day.cq.wcm.foundation.security.SaferSlingPostValidator;
         service = {Servlet.class, Filter.class},
         configurationPid = "com.adobe.cq.wcm.core.components.commons.forms.impl.CoreFormsHandlingServlet",
         property = {
-                "sling.servlet.resourceTypes=" + FormConstants.RT_CORE_FORM_CONTAINER_V1,
-                "sling.servlet.resourceTypes=" + FormConstants.RT_CORE_FORM_CONTAINER_V2,
-                "sling.servlet.methods=POST",
-                "sling.servlet.selectors=" + CoreFormHandlingServlet.SELECTOR,
-                "sling.servlet.extensions=" + CoreFormHandlingServlet.EXTENSION,
-                "sling.filter.scope=request",
-                "service.ranking:Integer=610",
+            ServletResolverConstants.SLING_SERVLET_RESOURCE_TYPES + "=" + FormConstants.RT_CORE_FORM_CONTAINER_V1,
+            ServletResolverConstants.SLING_SERVLET_RESOURCE_TYPES + "=" + FormConstants.RT_CORE_FORM_CONTAINER_V2,
+            ServletResolverConstants.SLING_SERVLET_METHODS + "=" + HttpConstants.METHOD_POST,
+            ServletResolverConstants.SLING_SERVLET_SELECTORS + "=" + CoreFormHandlingServlet.SELECTOR,
+            ServletResolverConstants.SLING_SERVLET_EXTENSIONS + "=" + CoreFormHandlingServlet.EXTENSION,
+            EngineConstants.SLING_FILTER_SCOPE + "=" + EngineConstants.FILTER_SCOPE_REQUEST,
+            Constants.SERVICE_RANKING + ":Integer=610",
         }
 )
-@Designate(
-        ocd = CoreFormHandlingServlet.Configuration.class
-)
-public class CoreFormHandlingServlet
-        extends SlingAllMethodsServlet
-        implements Filter {
+@Designate(ocd = CoreFormHandlingServlet.Configuration.class)
+public final class CoreFormHandlingServlet extends SlingAllMethodsServlet implements Filter {
 
-    @ObjectClassDefinition(
-            name = "Core Form Handling Servlet",
-            description = "Accepts posting to a core form container component and performs validations"
-    )
+    /**
+     * Core Form Handling Servlet Configuration.
+     */
+    @ObjectClassDefinition(name = "Core Form Handling Servlet",
+        description = "Accepts posting to a form container component and performs validations")
     @interface Configuration {
-        @AttributeDefinition(
-                name = "Parameter Name Whitelist",
-                description = "List of name expressions that will pass request validation. A validation error will occur " +
-                        "if any posted parameters are not in the whitelist and not defined on the form."
-        ) String[] name_whitelist() default {};
 
+        /**
+         * Get the parameter name whitelist.
+         *
+         * @return The list of whitelisted parameter names.
+         */
         @AttributeDefinition(
-                name = "Allow Expressoins",
-                description = "Evaluate expressions on form submissions."
-        ) boolean allow_expressions() default true;
+            name = "Parameter Name Whitelist",
+            description = "List of name expressions that will pass request validation. A validation error will occur " +
+                "if any posted parameters are not in the whitelist and not defined on the form.")
+        String[] name_whitelist() default {};
+
+        /**
+         * Check if expressions should be evaluated in form submissions.
+         *
+         * @return True if expressions should be evaluated, false if not.
+         */
+        @AttributeDefinition(name = "Allow Expressions", description = "Evaluate expressions on form submissions.")
+        boolean allow_expressions() default true;
     }
 
+    /**
+     * Form selector that must be present for the servlet to handle the request.
+     */
     static final String SELECTOR = "form";
-    static final String EXTENSION = "html";
-    private static final Boolean PROP_ALLOW_EXPRESSION_DEFAULT = true;
 
+    /**
+     * Extension that must be present for the servlet to handle the request.
+     */
+    static final String EXTENSION = "html";
+
+    /**
+     * The name of the request property containing the users reCaptcha challenge response token.
+     */
+    static final String RECAPTCHA_TOKEN_PARAMETER = "g-recaptcha-response";
+
+    /**
+     * The helper for processing forms.
+     */
     private transient FormsHandlingServletHelper formsHandlingServletHelper;
 
+    /**
+     * Request validator (Service to check Sling Post requests for unsafe constructs).
+     */
     @Reference
     private transient SaferSlingPostValidator validator;
 
+    /**
+     * Form structure helper factory service.
+     */
     @Reference
     private transient FormStructureHelperFactory formStructureHelperFactory;
 
-    @Activate
-    protected void activate(Configuration configuration) {
+    /**
+     * Recaptcha validator factory service.
+     */
+    @Reference
+    private transient CaptchaValidatorFactory recaptchaValidatorFactory;
 
-        String[] dataNameWhitelist = PropertiesUtil.toStringArray(configuration.name_whitelist());
-        boolean allowExpressions = PropertiesUtil.toBoolean(configuration.allow_expressions(), PROP_ALLOW_EXPRESSION_DEFAULT);
-        formsHandlingServletHelper = new FormsHandlingServletHelper(dataNameWhitelist, validator, FormConstants.RT_ALL_CORE_FORM_CONTAINER,
-            allowExpressions, formStructureHelperFactory);
+    /**
+     * Activate the service.
+     *
+     * @param configuration The service configuration.
+     */
+    @Activate
+    protected void activate(@NotNull final Configuration configuration) {
+        this.formsHandlingServletHelper = new FormsHandlingServletHelper(
+            configuration.name_whitelist(),
+            validator,
+            FormConstants.RT_ALL_CORE_FORM_CONTAINER,
+            configuration.allow_expressions(),
+            formStructureHelperFactory);
     }
 
     /**
      * @see org.apache.sling.api.servlets.SlingAllMethodsServlet#doPost(org.apache.sling.api.SlingHttpServletRequest, org.apache.sling.api.SlingHttpServletResponse)
      */
     @Override
-    protected void doPost(@NotNull SlingHttpServletRequest request, @NotNull final SlingHttpServletResponse response)
+    protected void doPost(@NotNull final SlingHttpServletRequest request, @NotNull final SlingHttpServletResponse response)
             throws ServletException, IOException {
-        formsHandlingServletHelper.doPost(request, response);
+        if (!this.captchaRequired(request) || this.validateCaptcha(request)) {
+            formsHandlingServletHelper.doPost(request, response);
+        } else {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        }
+    }
+
+    /**
+     * Check if captcha is required for the current request.
+     * A captcha is required if the form container policy requires it,
+     * or if the form container contains a button with captcha enabled.
+     *
+     * @param request The current request.
+     * @return True if captcha validation is required, false if not.
+     */
+    private boolean captchaRequired(@NotNull final SlingHttpServletRequest request) {
+        // supplier to check if required by policy
+        Supplier<Boolean> captchaRequiredByPolicy = () ->
+            Optional.ofNullable(request.getResourceResolver().adaptTo(ContentPolicyManager.class))
+                .map(policyManager -> policyManager.getPolicy(request.getResource(), request))
+                .map(ContentPolicy::getProperties)
+                .map(props -> props.get(Captcha.PN_CAPTCHA_REQUIRED, Boolean.FALSE))
+                .orElse(Boolean.FALSE);
+
+        Supplier<Boolean> captchaRequiredByContent = () ->
+            StreamSupport.stream(formStructureHelperFactory.getFormStructureHelper(request.getResource())
+                .getFormElements(request.getResource()).spliterator(), false)
+                .anyMatch(resource -> resource.isResourceType(FormConstants.RT_CORE_FORM_CAPTCHA_V1));
+
+        return captchaRequiredByPolicy.get() || captchaRequiredByContent.get();
+    }
+
+    /**
+     * Validates the captcha token included in the current request.
+     *
+     * @param request The current request.
+     * @return True if the captcha validates, false if it does not.
+     */
+    private boolean validateCaptcha(@NotNull final SlingHttpServletRequest request) {
+        return Optional.of(this.recaptchaValidatorFactory)
+            .flatMap(factory -> factory.getValidator(request.getResource()))
+            .map(val -> val.validate(request.getParameter(RECAPTCHA_TOKEN_PARAMETER)))
+            .orElse(Boolean.FALSE);
     }
 
     /**
      * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
      */
-    public void doFilter(final ServletRequest request,
-                         final ServletResponse response,
-                         final FilterChain chain)
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
             throws IOException, ServletException {
         formsHandlingServletHelper.handleFilter(request, response, chain, EXTENSION, SELECTOR);
     }
@@ -126,6 +216,7 @@ public class CoreFormHandlingServlet
     /**
      * @see Filter#init(FilterConfig)
      */
+    @Override
     public void init(final FilterConfig config) {
         // nothing to do!
     }
