b638c482a48be847f932110deed809dd677a0aa0
Kyle Giovannetti <kyle.giovannetti@dal.ca>
2020-08-20 13:38:33 -0300

    Update core form handling servlet to validate captcha
    Updates the `CoreFormHanldingServlet` to validate that a captcha
    response is present and valid if required, prior to processing the
    form POST request.
    
    If the captcha is required but not present, or invalid, then a
    `forbidden` status code is returned.
    
    Update tests for `CoreFormHandlingServlet`
    
    Updates tests to verify the proper handling of the request
    by the servlet when the captcha is:
     1) not required
     2) required by component property; and,
       a) is valid
       b) is not valid
     3) required by policy; and,
       a) is valid
       b) is not valid
    
    Add JavaDocs to undocumented members of `CoreFormHandlingServlet`
    
    Adds JavaDoc comments to all undocumented internal members of the
    `CoreFormHandlingServlet` class.

--- bundles/core/src/test/java/com/adobe/cq/wcm/core/components/internal/servlets/CoreFormHandlingServletTest.java
+++ bundles/core/src/test/java/com/adobe/cq/wcm/core/components/internal/servlets/CoreFormHandlingServletTest.java
@@ -15,74 +15,194 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package com.adobe.cq.wcm.core.components.internal.servlets;
 
-import java.lang.annotation.Annotation;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Optional;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import com.adobe.cq.wcm.core.components.internal.form.FormStructureHelperImpl;
+import com.day.cq.wcm.foundation.forms.FormStructureHelper;
+import org.apache.sling.scripting.api.resource.ScriptingResourceResolverProvider;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import com.adobe.cq.wcm.core.components.context.CoreComponentTestContext;
+import com.adobe.cq.wcm.core.components.internal.form.FormConstants;
+import com.adobe.cq.wcm.core.components.models.form.Captcha;
+import com.adobe.cq.wcm.core.components.services.captcha.CaptchaValidator;
+import com.adobe.cq.wcm.core.components.services.captcha.CaptchaValidatorFactory;
 import com.adobe.cq.wcm.core.components.testing.Utils;
 import com.day.cq.wcm.foundation.forms.FormsHandlingServletHelper;
+import com.day.cq.wcm.foundation.forms.FormStructureHelperFactory;
+import com.day.cq.wcm.foundation.security.SaferSlingPostValidator;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 @ExtendWith({AemContextExtension.class, MockitoExtension.class})
 public class CoreFormHandlingServletTest {
 
+    private static final String TEST_BASE = "/form/container";
+    private static final String CONTAINING_PAGE = "/content/coretest/demo-page";
+    private static final String FORM3_PATH = CONTAINING_PAGE + "/jcr:content/root/responsivegrid/container-v2";
+    private static final String FORM4_PATH = CONTAINING_PAGE + "/jcr:content/root/responsivegrid/container-v2-captcha-required";
+
+    private static final String RECAPTCHA_TOKEN_PARAMETER = "TOKEN_PARAMETER";
+
     private static final String[] NAME_WHITELIST = {"param-text", "param-button"};
-    private static final boolean ALLOW_EXPRESSIONS = false;
+    private static final Boolean ALLOW_EXPRESSIONS = Boolean.FALSE;
     private static final String SELECTOR = "form";
     private static final String EXTENSION = "html";
 
     @Mock
     FormsHandlingServletHelper formsHandlingServletHelper;
 
+    @Mock
+    private CaptchaValidatorFactory recaptchaValidatorFactory;
+
+    @Mock
+    private SaferSlingPostValidator saferSlingPostValidator;
+
     @InjectMocks
     CoreFormHandlingServlet servlet;
 
-    public static AemContext context = CoreComponentTestContext.newAemContext();
+    public final AemContext context = CoreComponentTestContext.newAemContext();
 
     @BeforeEach
     public void setUp() {
-        servlet = new CoreFormHandlingServlet();
-        servlet.activate(new CoreFormHandlingServlet.Configuration() {
+        context.load().json(TEST_BASE + CoreComponentTestContext.TEST_CONTENT_JSON, CONTAINING_PAGE);
 
-            @Override
-            public Class<? extends Annotation> annotationType() {
-                return null;
-            }
+        context.registerService(ScriptingResourceResolverProvider.class, context::resourceResolver);
+        FormStructureHelper helper = context.registerInjectActivateService(new FormStructureHelperImpl());
+        context.registerService(FormStructureHelperFactory.class, resource -> helper);
+        context.registerService(CaptchaValidatorFactory.class, recaptchaValidatorFactory);
+        context.registerService(SaferSlingPostValidator.class, saferSlingPostValidator);
 
-            @Override
-            public String[] name_whitelist() {
-                return NAME_WHITELIST;
-            }
+        this.servlet = this.context.registerInjectActivateService(new CoreFormHandlingServlet(), new HashMap<String, Object>() {{
+            put("name_whitelist", NAME_WHITELIST);
+            put("allow_expressions", ALLOW_EXPRESSIONS);
+        }});
 
-            @Override
-            public boolean allow_expressions() {
-                return ALLOW_EXPRESSIONS;
-            }
-        });
         Utils.setInternalState(servlet, "formsHandlingServletHelper", formsHandlingServletHelper);
     }
 
     @Test
+    @DisplayName("Captcha not required")
     public void testDoPost() throws Exception {
+        context.currentResource(this.context.resourceResolver().getResource(FORM3_PATH));
         servlet.doPost(context.request(), context.response());
+
+        assertEquals(HttpServletResponse.SC_OK, context.response().getStatus());
         verify(formsHandlingServletHelper).doPost(context.request(), context.response());
+    }
+
+    @Nested
+    class CaptchaRequired {
+
+        CaptchaValidator validator;
+
+        @BeforeEach
+        public void setUp() {
+            this.validator = spy(new MockValidator());
+        }
+
+        @Test
+        @DisplayName("Captcha required as per component properties, invalid token provided")
+        public void testDoPost_captchaRequired_invalid() throws Exception {
+            doReturn(Optional.of(this.validator)).when(recaptchaValidatorFactory).getValidator(any());
+            context.currentResource(context.resourceResolver().getResource(FORM4_PATH));
+            servlet.doPost(context.request(), context.response());
+
+            assertEquals(HttpServletResponse.SC_FORBIDDEN, context.response().getStatus());
+            verify(formsHandlingServletHelper, never()).doPost(context.request(), context.response());
+            verify(validator).validate(any(HttpServletRequest.class));
+        }
+
+        @Test
+        @DisplayName("Captcha required as per component properties, valid token provided")
+        public void testDoPost_captchaRequired_valid() throws Exception {
+            doReturn(Optional.of(this.validator)).when(recaptchaValidatorFactory).getValidator(any());
+            context.request().setParameterMap(Collections.singletonMap(RECAPTCHA_TOKEN_PARAMETER, "valid_response"));
+
+            context.currentResource(context.resourceResolver().getResource(FORM4_PATH));
+            servlet.doPost(context.request(), context.response());
+
+            assertEquals(HttpServletResponse.SC_OK, context.response().getStatus());
+            verify(formsHandlingServletHelper).doPost(context.request(), context.response());
+            verify(validator).validate(eq("valid_response"));
+        }
+
+        @Test
+        @DisplayName("Captcha required as per policy, no captcha components")
+        public void testDoPost_captchaRequired_by_policy_no_captcha_components() throws Exception {
+            context.contentPolicyMapping(FormConstants.RT_CORE_FORM_CONTAINER_V2, new HashMap<String, Object>() {{
+                put(Captcha.PN_CAPTCHA_REQUIRED, Boolean.TRUE);
+            }});
+
+            context.request().setParameterMap(Collections.singletonMap(RECAPTCHA_TOKEN_PARAMETER, "valid_response"));
+
+            context.currentResource(context.resourceResolver().getResource(FORM3_PATH));
+            servlet.doPost(context.request(), context.response());
+
+            assertEquals(HttpServletResponse.SC_FORBIDDEN, context.response().getStatus());
+            verify(formsHandlingServletHelper, never()).doPost(context.request(), context.response());
+            verifyZeroInteractions(validator);
+        }
+
+        @Test
+        @DisplayName("Captcha required as per policy, valid token provided")
+        public void testDoPost_captchaRequired_by_policy_valid() throws Exception {
+            doReturn(Optional.of(this.validator)).when(recaptchaValidatorFactory).getValidator(any());
+            context.contentPolicyMapping(FormConstants.RT_CORE_FORM_CONTAINER_V2, new HashMap<String, Object>() {{
+                put(Captcha.PN_CAPTCHA_REQUIRED, Boolean.TRUE);
+            }});
+
+            context.request().setParameterMap(Collections.singletonMap(RECAPTCHA_TOKEN_PARAMETER, "valid_response"));
+
+            context.currentResource(context.resourceResolver().getResource(FORM4_PATH));
+            servlet.doPost(context.request(), context.response());
+
+            assertEquals(HttpServletResponse.SC_OK, context.response().getStatus());
+            verify(formsHandlingServletHelper).doPost(context.request(), context.response());
+            verify(validator).validate(eq("valid_response"));
+        }
     }
 
     @Test
     public void testDoFilter() throws Exception {
+        servlet.init(mock(FilterConfig.class));
+
         FilterChain filterChain = mock(FilterChain.class);
         servlet.doFilter(context.request(), context.response(), filterChain);
         verify(formsHandlingServletHelper).handleFilter(context.request(), context.response(), filterChain, EXTENSION, SELECTOR);
+    }
+
+
+    static final class MockValidator implements CaptchaValidator {
+
+        @Override
+        public boolean validate(@Nullable String userResponse) {
+            return "valid_response".equals(userResponse);
+        }
+
+        @Override
+        public boolean validate(@NotNull HttpServletRequest request) {
+            return this.validate(request.getParameter(RECAPTCHA_TOKEN_PARAMETER));
+        }
     }
 }
